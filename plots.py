2d2cc771117eefc01af1b38f86d1ca074cf7b563
MuneebKhan46 <81796045+MuneebKhan46@users.noreply.github.com>
2024-10-29 18:01:22 +0900

    Update plots.py

--- plots.py
+++ plots.py
@@ -2,41 +2,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix, roc_curve, auc
 
-def plot_confusion_matrix(y_true, y_pred, classes):
-    """
-    Plot the confusion matrix using Seaborn heatmap.
-
-    Args:
-        y_true: True labels.
-        y_pred: Predicted labels.
-        classes: List of class names.
-    """
+def plot_confusion_matrix(y_true, y_pred, class_names):
+    """Plot the confusion matrix."""
     cm = confusion_matrix(y_true, y_pred)
     plt.figure(figsize=(8, 6))
-    sns.heatmap(cm, annot=True, cmap='Blues', fmt='d', xticklabels=classes, yticklabels=classes)
+    sns.heatmap(cm, annot=True, cmap='Blues', fmt='d', xticklabels=class_names, yticklabels=class_names)
     plt.xlabel('Predicted Label')
     plt.ylabel('True Label')
     plt.title('Confusion Matrix')
     plt.show()
 
-def plot_roc_curve(y_true, y_pred_probs, classes):
-    """
-    Plot the ROC curve for multi-class classification.
-
-    Args:
-        y_true: One-hot encoded true labels.
-        y_pred_probs: Predicted probabilities for each class.
-        classes: List of class names.
-    """
+def plot_roc_curve(y_true, y_pred_probs, class_names):
+    """Plot ROC curves for each class."""
     plt.figure(figsize=(10, 8))
-    for i, class_name in enumerate(classes):
+    for i, class_name in enumerate(class_names):
         fpr, tpr, _ = roc_curve(y_true[:, i], y_pred_probs[:, i])
-        roc_auc = auc(fpr, tpr)
-        plt.plot(fpr, tpr, lw=2, label=f'ROC for {class_name} (AUC = {roc_auc:.2f})')
-
-    plt.plot([0, 1], [0, 1], 'k--', lw=2)
+        plt.plot(fpr, tpr, label=f'{class_name} (AUC = {auc(fpr, tpr):.2f})')
+    plt.plot([0, 1], [0, 1], 'k--')
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title('Multi-Class ROC Curve')
+    plt.title('ROC Curve')
     plt.legend(loc='lower right')
     plt.show()
